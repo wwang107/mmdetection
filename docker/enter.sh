18bbd2d845c907003cf3ddb1bc88ae2c898a094c
Wei Wang <weiwang@pop-os.localdomain>
2020-04-19 16:35:33 +0200

    Map human and pretrained weight

--- docker/enter.sh
+++ docker/enter.sh
@@ -2,6 +2,8 @@
 sudo docker run --gpus all\
   --privileged\
   -it\
+  -v "/media/weiwang/Elements/human36m":/mmdetection/data/human36m\
+  -v "../checkpoints":/mmdetection/checkpoints\
   -p 8888:8888\
   weiwang/bbextractor\
   /bin/bash

6fce2aa015141bc6f05f57d9abb395268e397af6
Wei Wang <weiwang@pop-os.localdomain>
2020-04-19 16:41:03 +0200

    Fix pretrained weight path

--- docker/enter.sh
+++ docker/enter.sh
@@ -3,7 +3,7 @@ sudo docker run --gpus all\
   --privileged\
   -it\
   -v "/media/weiwang/Elements/human36m":/mmdetection/data/human36m\
-  -v "/home/weiwang/Desktop/master-thesis/mmdetection/checkpoints/faster_rcnn_r50_fpn_1x_20181010-3d1b3351.pth":/mmdetection/checkpoints\
+  -v "/home/weiwang/Desktop/master-thesis/mmdetection/checkpoints/":/mmdetection/checkpoints\
   -p 8888:8888\
   weiwang/bbextractor\
   /bin/bash
